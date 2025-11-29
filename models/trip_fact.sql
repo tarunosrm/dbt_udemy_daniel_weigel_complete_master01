44fd6a41f6903b40d9b3617fea0797fbaf78d04b
Tarun sharma <linktarun01@gmail.com>
2025-11-29 17:59:29 +0000

    trip_fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,27 @@
+--*** DBT - Trip fact ***
+
+-- Documentation on the TIMESTAMPDIFF function in snowflake: https://docs.snowflake.com/en/sql-reference/functions/timestampdiff
+
+
+-- Code used for trip_fact.sql model:
+
+WITH TRIPS as (
+
+select
+RIDE_ID,
+RIDEABLE_TYPE,
+DATE(TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE,
+START_STATIO_ID AS START_STATION_ID,
+END_STATION_ID,
+MEMBER_CSUAL AS MEMBER_CASUAL,
+TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(STARTED_AT),TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION_SECONDS
+
+from {{ source('demo', 'bike') }}
+
+where RIDE_ID != 'ride_id'
+
+)
+
+select
+*
+from TRIPS
